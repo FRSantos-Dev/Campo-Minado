969a8e170bd8b1dfbcddbfa480cef29d88bf1205
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-07-13 20:18:34 -0300

    Function open and safe neighbourhood

--- src/br/com/rocha/cm/model/Camp.java
+++ src/br/com/rocha/cm/model/Camp.java
@@ -3,6 +3,8 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import test.br.com.rocha.cm.model.exception.ExplosionException;
+
 public class Camp {
 
     private final int line;
@@ -18,7 +20,7 @@ public class Camp {
         this.line = line;
         this.column = column;
     }
-    
+
     boolean addNeighbour(Camp neighbour) {
         boolean differentLine = line != neighbour.line;
         boolean differentColumn = column != neighbour.column;
@@ -28,15 +30,42 @@ public class Camp {
         int deltaColumn = Math.abs(column - neighbour.column);
         int deltaGeneral = deltaColumn + deltaLine;
 
-        if(deltaGeneral == 1 && !diagonal){
+        if (deltaGeneral == 1 && !diagonal) {
             neighbours.add(neighbour);
             return true;
-        }else if(deltaGeneral == 2 && diagonal){
+        } else if (deltaGeneral == 2 && diagonal) {
             neighbours.add(neighbour);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
+
+    void alternateMark() {
+        if (!open) {
+            marked = !marked;
+        }
+    }
+
+    boolean open() {
+        
+        if (!open && !marked) {
+            open = true;
+            
+            if (mined) {
+                throw new ExplosionException()
+            }
+
+            if(neighbourshoodSafe()){
+                neighbours.forEach(v -> v.open());
+            }
+            return true;
+        }else{
+        return false;
+        }
+    }
+
+    boolean safeNeighbourhood() {
+        return neighbours.stream().noneMatch(v -> v.mined);
+    }
 }
--- test/br/com/rocha/cm/model/CampTest.java
+++ test/br/com/rocha/cm/model/CampTest.java
@@ -8,16 +8,16 @@ import org.junit.Test;
 import model.Camp;
 
 public class CampTest {
-    
+
     private Camp camp;
 
     @Before
-    void initiateCamp(){
+    void initiateCamp() {
         camp = new Camp(3, 3);
     }
 
     @Test
-    void testRealNeighbourDistance1(){
+    void testRealNeighbourDistance1() {
         Camp neighbour = new Camp(3, 2);
         boolean result = camp.addNeighbour(neighbour);
         assertTrue(result);
@@ -25,28 +25,28 @@ public class CampTest {
     }
 
     @Test
-    void testRealNeighbourDistance1Left(){
+    void testRealNeighbourDistance1Left() {
         Camp neighbour = new Camp(3, 2);
         boolean result = camp.addNeighbour(neighbour);
         assertTrue(result);
     }
 
     @Test
-    void testRealNeighbourDistance1Right(){
+    void testRealNeighbourDistance1Right() {
         Camp neighbour = new Camp(3, 4);
         boolean result = camp.addNeighbour(neighbour);
         assertTrue(result);
     }
 
     @Test
-    void testRealNeighbourDistance1Top(){
+    void testRealNeighbourDistance1Top() {
         Camp neighbour = new Camp(2, 3);
         boolean result = camp.addNeighbour(neighbour);
         assertTrue(result);
     }
 
     @Test
-    void testRealNeighbourDistance1Bottom(){
+    void testRealNeighbourDistance1Bottom() {
         Camp neighbour = new Camp(2, 3);
         boolean result = camp.addNeighbour(neighbour);
         assertTrue(result);
