1c4456789e2a61c05a1684916dde314e8cc429f0
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-08-14 16:32:16 -0300

    Board Construction Part II

--- src/br/com/rocha/cm/model/Board.java
+++ src/br/com/rocha/cm/model/Board.java
@@ -2,9 +2,10 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Board {
-    
+
     private int lines;
     private int columns;
     private int mines;
@@ -21,22 +22,46 @@ public class Board {
         raffleMines();
     }
 
-        private void generateCamps(){
-            for(int line = 0; line < lines; line++){
-                for(int column = 0; column < columns; column++){
-                    camps.add(new Camp(line, column));
-                }
+    private void generateCamps() {
+        for (int line = 0; line < lines; line++) {
+            for (int column = 0; column < columns; column++) {
+                camps.add(new Camp(line, column));
             }
+        }
     }
 
-        private void associateNeighbours() {
-            for(Camp c1: camps){
-                for(Camp c2: camps){
-                    c1.addNeighbour(c2);
-                }
+    private void associateNeighbours() {
+        for (Camp c1 : camps) {
+            for (Camp c2 : camps) {
+                c1.addNeighbour(c2);
             }
+        }
     }
 
-        private void raffleMines() {
+    private void raffleMines() {
+        long armedMines = 0;
+        Predicate<Camp> mined = c -> c.isMined();
+
+        do {
+            armedMines = camps.stream().filter(mined).count();
+            int aleatory = (int) (Math.random() * camps.size());
+            camps.get(aleatory).mined();
+        } while (armedMines < mines);
     }
+
+    public boolean goalCompleted() {
+        return camps.stream().allMatch(c -> c.goalCompleted());
+    }
+
+    public void restart() {
+        camps.stream().forEach(c -> c.restart());
+        raffleMines();
+    }
+
+    @Override
+    public String toString() {
+        return "Board [lines=" + lines + ", columns=" + columns
+                + ", mines=" + mines + ", camps=" + camps + "]";
+    }
+
 }
