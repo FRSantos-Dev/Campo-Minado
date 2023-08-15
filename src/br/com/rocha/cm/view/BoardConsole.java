f72c55b58eff1bb66099b512ad4fb386217774c1
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-08-14 21:46:14 -0300

    Methods created, gameCycle and captureTypedValue

--- src/br/com/rocha/cm/view/BoardConsole.java
+++ src/br/com/rocha/cm/view/BoardConsole.java
@@ -1,7 +1,11 @@
 package view;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import exception.ExplosionException;
 import exception.SignOutException;
 import model.Board;
 
@@ -25,16 +29,51 @@ public class BoardConsole {
                 System.out.println("Another game? (S/n) ");
                 String answer = entry.nextLine();
 
-                if("n".equalsIgnoreCase(answer)){
+                if ("n".equalsIgnoreCase(answer)) {
                     continueGame = false;
                 } else {
                     board.restart();
                 }
             }
-        }catch(SignOutException e){
+        } catch (SignOutException e) {
             System.out.println("See you later!");
-        }finally{
+        } finally {
             entry.close();
         }
-    }   
+    }
+
+    private void gameCycle() {
+        try {
+            while (!board.goalCompleted()) {
+                System.out.println(board);
+
+                String typed = captureTypedValue("Type (x, y): ");
+
+                java.util.Iterator<Integer> xy = Arrays.stream(typed.split(","))
+                        .map(e -> Integer.parseInt(e.trim())).iterator();
+
+                typed = captureTypedValue("1 - Open or 2 - (Un)Mark: ");
+
+                if("1".equals(typed)){
+                    board.open(xy.next(), xy.next());
+                } else if("2".equals(typed)){
+                    board.alternateMark(xy.next(), xy.next());
+                }
+            }
+            System.out.println("You win !!");
+        } catch (ExplosionException e) {
+            System.out.println(board);
+            System.out.println("You lose!");
+        }
+    }
+
+    private String captureTypedValue(String text) {
+        System.out.println(text);
+        String typed = entry.nextLine();
+
+        if ("exit".equalsIgnoreCase(typed)) {
+            throw new SignOutException();
+        }
+        return typed;
+    }
 }
